9ceaff0488a8768f90396e8076ef12cbb514fa77
Viacheslav Malykhin <vyacheslav.malykhin@clockwise.software>
2024-09-24 11:38:56 +0300

    Setup I18N

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
 	import { setContext } from 'svelte';
+	import { _, locale, waitLocale, isLoading } from 'svelte-i18n';
+	import { browser } from '$app/environment';
+	import '$lib/i18n';
 	import client from '$lib/apolloClient';
+	import LangSwitcher from '../components/LangSwitcher.svelte';
 
 	setContext('apollo', client);
+
+	export const load = async () => {
+		if (browser) {
+			locale.set(window.navigator.language);
+		}
+		await waitLocale();
+	};
 </script>
+
+<LangSwitcher />
+
+{#if $isLoading}
+	<p>Wait... Lang loading</p>
+{:else}
+	<p>{$_('hello')}</p>
+{/if}
 
 <slot />
