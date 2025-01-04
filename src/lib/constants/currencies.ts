d7aeec1288ab0618e13746ddcb8dd3c3bb4e5ffe
Viacheslav Malykhin <vyacheslav.malykhin@clockwise.software>
2025-01-04 03:48:50 +0200

    Remove Svelte files

--- src/lib/constants/currencies.ts
+++
@@ -1,9 +0,0 @@
-export const CURRENCIES = ['USD', 'EUR', 'UAH', 'PLN', 'MDL', 'RON'];
-export const CURRENCIES_SYMBOL = {
-	USD: '$',
-	EUR: '€',
-	UAH: '₴',
-	PLN: 'zł',
-	MDL: 'L',
-	RON: 'lei'
-};
--- src/lib/store/me.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-import type { AwaitedReturn } from '../../utils/types';
-import AuthService from '$lib/services/auth-service';
-
-export const me = writable<AwaitedReturn<typeof AuthService.me>>(null);
